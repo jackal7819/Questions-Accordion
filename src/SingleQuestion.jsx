bbb8372679083d735b16fc3b750ebc2392cab8d4
Viktor Filippov <jackal3166@gmail.com>
2023-09-27 14:45:54 +0300

    Add showInfo

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -4,16 +4,24 @@ import { useState } from 'react';
 
 const SingleQuestion = ({ id, title, info }) => {
     const [showInfo, setShowInfo] = useState(false);
-    
+
+    const toggleShowInfo = () => {
+        setShowInfo(!showInfo);
+    };
+
     return (
         <article className='question'>
             <header>
                 <h5>
                     {id}. {title}
                 </h5>
+                <button className='question-btn' onClick={toggleShowInfo}>
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                </button>
             </header>
-            <p>{info}</p>
+            <p>{showInfo && info}</p>
         </article>
     );
 };
+
 export default SingleQuestion;
--- src/data.js
+++ src/data.js
@@ -1,27 +1,27 @@
 export const data = [
     {
         id: 1,
-        title: 'What was the first home gaming console released by Nintendo?',
-        info: 'The first home gaming console released by Nintendo was the Nintendo Entertainment System (NES), also known as the Famicom in Japan.',
+        title: 'What was Nintendo\'s first home gaming console?',
+        info: 'Nintendo\'s first home gaming console was the Nintendo Entertainment System (NES), also known as the Famicom in Japan. It was released in 1983.'
     },
     {
         id: 2,
-        title: 'Which Nintendo console introduced the concept of portable gaming with interchangeable game cartridges?',
-        info: 'The concept of portable gaming with interchangeable game cartridges was introduced with the Nintendo Game Boy.',
+        title: 'Which Nintendo console introduced portable gaming with interchangeable cartridges?',
+        info: 'Portable gaming with interchangeable cartridges was introduced by the Nintendo Game Boy. It was released in 1989 and became highly popular.'
     },
     {
         id: 3,
-        title: 'What was the first 16-bit gaming console by Nintendo?',
-        info: 'The first 16-bit gaming console by Nintendo was the Super Nintendo Entertainment System (SNES), also known as the Super Famicom in Japan.',
+        title: 'What was Nintendo\'s first 16-bit gaming console?',
+        info: 'Nintendo\'s first 16-bit gaming console was the Super Nintendo Entertainment System (SNES), also known as the Super Famicom in Japan. It was released in 1990.'
     },
     {
         id: 4,
-        title: 'Which Nintendo console featured innovative motion controls and a touchscreen?',
-        info: 'The Nintendo Wii featured innovative motion controls with the Wii Remote and a touchscreen with the Wii U GamePad.',
+        title: 'Which Nintendo console featured motion controls and a touchscreen?',
+        info: 'The Nintendo Wii featured innovative motion controls with the Wii Remote and a touchscreen with the Wii U GamePad. It was released in 2006.'
     },
     {
         id: 5,
-        title: 'What is the latest gaming console released by Nintendo as of 2023?',
-        info: 'As of 2023, the latest gaming console released by Nintendo is the Nintendo Switch, which can be used both as a handheld and a home console.',
-    },
+        title: 'What is Nintendo\'s latest gaming console as of 2022?',
+        info: 'As of 2022, Nintendo\'s latest gaming console is the Nintendo Switch, which can be used both as a handheld and a home console. It was released in 2017 and has been a major success.'
+    }
 ];
