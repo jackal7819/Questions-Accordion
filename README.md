573443f603b7e3fa315dc99dc22fab7753974ca0
Viktor Filippov <jackal3166@gmail.com>
2023-09-27 13:40:06 +0300

    Create App

---
+++ README.md
@@ -0,0 +1,25 @@
+# Questions Accordion
+
+[Project Link](project_link)
+
+A simple React project that allows to display a list of questions in an accordion format, enabling users to expand and collapse answers to the questions.
+
+## Technologies
+
+- React
+- JavaScript
+- CSS (for styling)
+- [Vite](https://vitejs.dev/): Build tool for fast development and production.
+
+## Description
+
+This project is designed to display a set of questions and answers in a convenient accordion format. Users can click on questions to reveal answers and collapse them again, making information viewing more user-friendly and interactive.
+
+## Additional Features
+
+The project also includes additional features such as:
+
+- The ability to display only one answer at a time (the selected question).
+- The ability to collapse answers when clicking on a question.
+
+Enjoy working with the project!
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const questions = [
+    {
+        id: 1,
+        title: 'What was the first home gaming console released by Nintendo?',
+        info: 'The first home gaming console released by Nintendo was the Nintendo Entertainment System (NES), also known as the Famicom in Japan.',
+    },
+    {
+        id: 2,
+        title: 'Which Nintendo console introduced the concept of portable gaming with interchangeable game cartridges?',
+        info: 'The concept of portable gaming with interchangeable game cartridges was introduced with the Nintendo Game Boy.',
+    },
+    {
+        id: 3,
+        title: 'What was the first 16-bit gaming console by Nintendo?',
+        info: 'The first 16-bit gaming console by Nintendo was the Super Nintendo Entertainment System (SNES), also known as the Super Famicom in Japan.',
+    },
+    {
+        id: 4,
+        title: 'Which Nintendo console featured innovative motion controls and a touchscreen?',
+        info: 'The Nintendo Wii featured innovative motion controls with the Wii Remote and a touchscreen with the Wii U GamePad.',
+    },
+    {
+        id: 5,
+        title: 'What is the latest gaming console released by Nintendo as of 2023?',
+        info: 'As of 2023, the latest gaming console released by Nintendo is the Nintendo Switch, which can be used both as a handheld and a home console.',
+    },
+];
